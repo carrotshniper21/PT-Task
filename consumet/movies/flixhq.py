eb8d3c89c8ca87f290a454c84e073bbee59ab8e3
carrotshniper21 <vipershniper07@gmail.com>
2024-03-22 03:44:30 -0500

    feat: update ui & add trending page

---
+++ consumet/movies/flixhq.py
@@ -0,0 +1,207 @@
+import requests
+from bs4 import BeautifulSoup
+from enum import Enum
+from pydantic import BaseModel
+from typing import List, Optional, Tuple
+from consumet.html.flixhq_html import SearchParser, PageParser
+import concurrent.futures
+
+class MediaType(Enum):
+    TV = "tv"
+    MOVIE = "movie"
+
+class FlixHQResult(BaseModel):
+    Id: str
+    Cover: str
+    Title: str
+    Url: str
+    Image: str
+    ReleaseDate: str
+    MediaType: MediaType
+    Genres: List[str]
+    Description: str
+    Rating: str
+    Quality: str
+    Duration: str
+    Country: List[str]
+    Production: List[str]
+    Cast: List[str]
+    Tags: List[str]
+
+class FlixHQSearchResults(BaseModel):
+    CurrentPage: int
+    HasNextPage: bool
+    TotalPages: int
+    TotalResults: int
+    Results: List[FlixHQResult]
+
+class FlixHQHTML:
+    def parse_search(self, page_html: str) -> Tuple[List[str], bool, int]:
+        soup = BeautifulSoup(page_html, "html.parser")
+        search_parser = SearchParser(soup)
+        return ( search_parser.page_ids(), search_parser.has_next_page(), search_parser.total_pages()) 
+
+    def single_page(self, media_html: str, id: str, url: str) -> FlixHQResult: 
+        soup = BeautifulSoup(media_html, "html.parser")
+        page_parser = PageParser(soup)
+
+        result = FlixHQResult(
+            Id=id,
+            Cover=page_parser.cover(),
+            Title=page_parser.title(),
+            Url=url,
+            Image=page_parser.image(),
+            ReleaseDate=page_parser.label(3, "Released:")[0],
+            MediaType=page_parser.media_type(id),
+            Genres=page_parser.label(2, "Genre:"),
+            Description=page_parser.description(),
+            Rating=page_parser.rating(),
+            Quality=page_parser.quality(),
+            Duration=page_parser.duration(),
+            Country=page_parser.label(1, "Country:"),
+            Production=page_parser.label(4, "Production:"),
+            Cast=page_parser.label(5, "Casts:"),
+            Tags=page_parser.label(6, "Tags:")
+        )
+
+        return result
+
+    def parse_trending_movies(self, page_html: str) -> List[str]:
+        soup = BeautifulSoup(page_html, "html.parser")
+        page_parser = SearchParser(soup)
+
+        return page_parser.trending_movies()
+
+    def parse_trending_shows(self, page_html: str) -> List[str]:
+        soup = BeautifulSoup(page_html, "html.parser")
+        page_parser = SearchParser(soup)
+
+        return page_parser.trending_shows()
+                
+class FlixHQ:
+    """
+    Finds film details, parses data and returns film info
+    """
+
+    def __init__(self):
+        self.base_url = "https://flixhq.to"
+
+    def load_url(self, url, timeout=5):
+        try:
+            response = requests.get(url, timeout=timeout).text
+            return response
+        except Exception as e:
+            print(f"Error fetching URL {url}: {e}")
+            return None
+
+    async def search(self, query: str, page: Optional[int]) -> FlixHQSearchResults:
+        page = int(page) if page else 1
+
+        searchHtml = requests.get(
+            f"{self.base_url}/search/{query.replace(' ', '-')}?page={page}", timeout=10
+        ).text
+
+        flixhq_parser = FlixHQHTML()
+
+        (ids, has_next_page, total_pages) = flixhq_parser.parse_search(searchHtml)
+
+        urls = []
+
+        for id in ids:
+            url = f'{self.base_url}/{id}'
+            urls.append(url)
+
+        html = []
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=30) as executor:
+            future_to_url = (executor.submit(self.load_url, url, 5) for url in urls)
+            for future in concurrent.futures.as_completed(future_to_url):
+                try:
+                    data = future.result()
+                except Exception as exc:
+                    data = str(type(exc))
+                finally:
+                    html.append(data)
+
+        results = []
+
+        for i, media_html in enumerate(html):
+            result = flixhq_parser.single_page(media_html, id, str(urls[i]))
+            results.append(result)
+
+        filmResponse = FlixHQSearchResults(
+            CurrentPage=page,
+            HasNextPage=has_next_page,
+            TotalPages=total_pages,
+            TotalResults=len(ids),
+            Results=results
+        )
+
+        return filmResponse.dict()
+
+    async def trending_movies(self) -> List[FlixHQResult]:
+        trendingHtml = requests.get(
+            f"{self.base_url}/home"
+        )
+        flixhq_parser = FlixHQHTML()
+        ids = flixhq_parser.parse_trending_movies(trendingHtml.text)
+
+        urls = []
+
+        for id in ids:
+            url = f'{self.base_url}/{id}'
+            urls.append(url)
+
+        html = []
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=30) as executor:
+            future_to_url = (executor.submit(self.load_url, url, 5) for url in urls)
+            for future in concurrent.futures.as_completed(future_to_url):
+                try:
+                    data = future.result()
+                except Exception as exc:
+                    data = str(type(exc))
+                finally:
+                    html.append(data)
+
+        results = []
+
+        for i, media_html in enumerate(html):
+            result = flixhq_parser.single_page(media_html, id, str(urls[i]))
+            results.append(result.dict())
+
+        return results
+
+
+    async def trending_shows(self) -> List[FlixHQResult]:
+        trendingHtml = requests.get(
+            f"{self.base_url}/home"
+        )
+        flixhq_parser = FlixHQHTML()
+        ids = flixhq_parser.parse_trending_shows(trendingHtml.text)
+
+        urls = []
+
+        for id in ids:
+            url = f'{self.base_url}/{id}'
+            urls.append(url)
+
+        html = []
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=30) as executor:
+            future_to_url = (executor.submit(self.load_url, url, 5) for url in urls)
+            for future in concurrent.futures.as_completed(future_to_url):
+                try:
+                    data = future.result()
+                except Exception as exc:
+                    data = str(type(exc))
+                finally:
+                    html.append(data)
+
+        results = []
+
+        for i, media_html in enumerate(html):
+            result = flixhq_parser.single_page(media_html, id, str(urls[i]))
+            results.append(result.dict())
+
+        return results
--- main.py
+++ main.py
@@ -1,56 +1,170 @@
-from consumet.flixhq.flixhq import FlixHQ
+from consumet.movies.flixhq import FlixHQ
 import PySimpleGUI as sg
+from PIL import Image
+import requests
+from io import BytesIO
+import os
 import asyncio
 
 # Function to fetch movie data via FlixHQ
-async def search_movies(search_term):
+async def search_movies(search_term, page):
     flixhq = FlixHQ()
-    response = await flixhq.search(search_term, None)
-    return response['Results']
+    response = await flixhq.search(search_term, page)
+    return response
 
-# Function to display search results
-async def display_results(results):
+async def get_trending_movies():
+    flixhq = FlixHQ()
+    response = await flixhq.trending_movies()
+    return response
+
+# Function to display movie details
+def display_movie_details(movie):
+    image_url = movie['Image']
+    
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    
+    response = requests.get(image_url, headers=headers)
+    
+    image = Image.open(BytesIO(response.content)).resize((100, 150))  # Resizing the image
+    bio = BytesIO()
+    image.save(bio, format="PNG")
+    image_data = bio.getvalue()
+
     layout = [
-        [sg.Text("Search Results", font=("Helvetica", 16), text_color="white")],
+        [sg.Text(movie['Title'], font=("Helvetica", 20), text_color="white", background_color="#2a2a2a", pad=((20,0),20))],
         [sg.HorizontalSeparator(color="white")],
-        [sg.Column([[sg.Text(result['Title'], size=(20, 1), text_color="white", font=("Helvetica", 12), justification="center")]],
-                   element_justification='center', pad=(10, 10))
-         for i, result in enumerate(results)]
+        [sg.Image(data=image_data), sg.Text(movie['Description'], font=("Helvetica", 14), text_color="white", background_color="#2a2a2a", size=(50, 5), auto_size_text=True)],
+        [sg.Button("Back", font=("Helvetica", 14), button_color=("white", "#007bff"), pad=((10,20),20))]
     ]
 
-    window = sg.Window("Movie Search Results", layout, background_color="black")
+    window = sg.Window("Movie Details", layout, size=(720, 520), background_color="#2a2a2a", finalize=True)
 
     while True:
         event, values = window.read()
 
-        if event == sg.WINDOW_CLOSED:
+        if event == sg.WINDOW_CLOSED or event == "Back":
             break
 
     window.close()
 
-async def main():
-    sg.theme('DarkBlack')
-
+# Function to display search results
+def display_results(results):
     layout = [
-        [sg.Text("Movie Search", font=("Helvetica", 16), text_color="white")],
-        [sg.InputText(key='search_term', size=(30, 1)), sg.Button("Search")],
+        [sg.Text("Search Results", font=("Helvetica", 20), text_color="white", background_color="#2a2a2a", pad=((20,0),20))],
+        [sg.HorizontalSeparator(color="white")],
     ]
 
-    window = sg.Window("Movie Search", layout, background_color="black")
+    movie_layout = []
+
+    for movie in results['Results']:
+        image_url = movie['Image']
+        
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+        
+        response = requests.get(image_url, headers=headers)
+        
+        image = Image.open(BytesIO(response.content)).resize((100, 150))  # Resizing the image
+        bio = BytesIO()
+        image.save(bio, format="PNG")
+        image_data = bio.getvalue()
+
+        movie_layout.append([
+            sg.Image(data=image_data, key=f"image_{movie['Id']}", enable_events=True),
+            sg.Text(movie['Title'], font=("Helvetica", 12), text_color="white", background_color="#2a2a2a", key=f"title_{movie['Id']}"),
+            sg.Text(movie['Description'], font=("Helvetica", 10), text_color="white", background_color="#2a2a2a", visible=False, key=f"description_{movie['Id']}")
+        ])
+
+    movie_layout.append([sg.Button("Close", font=("Helvetica", 14), button_color=("white", "#007bff"), pad=((10,20),20))])
+
+    layout.append([
+        sg.Column(movie_layout, size=(800, 400), scrollable=True, vertical_scroll_only=True)
+    ])
+
+    window = sg.Window("Movie Search Results", layout, size=(1080, 720), background_color="#2a2a2a", finalize=True)
+
+    while True:
+        event, values = window.read()
+
+        if event == sg.WINDOW_CLOSED or event == "Close":
+            break
+        elif event.startswith("image_"):
+            movie_id = event.split("_")[1]
+            selected_movie = next(movie for movie in results['Results'] if movie['Id'] == movie_id)
+            display_movie_details(selected_movie)
+
+    window.close()
+
+
+async def main():
+    sg.set_options(dpi_awareness=True)
+    sg.theme('DarkGrey7')
+
+    icon_path = os.path.join(os.getcwd(), "icon.ico")  # Using relative path
+
+    layout = [
+        [sg.Text("Movie Search", font=("Helvetica", 24), text_color="white", background_color="#2a2a2a", pad=((20,0),20))],
+        [sg.InputText(key='search_term', size=(30, 1), font=("Helvetica", 14), background_color="#3a3a3a", text_color="white", border_width=0, pad=((700,0),0), enable_events=True)],  
+    ]
+
+    movie_layout = []
+
+    results = await get_trending_movies()
+
+    movie_layout.append([
+        sg.Text("Trending Movies", font=("Helvetica", 20), text_color="white", background_color="#2a2a2a")
+    ])
+    movie_layout.append([
+        sg.HorizontalSeparator(color="white")
+    ])
+
+    for movie in results:
+        image_url = movie['Image']
+        
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+        
+        response = requests.get(image_url, headers=headers)
+        
+        image = Image.open(BytesIO(response.content)).resize((100, 150))  # Resizing the image
+        bio = BytesIO()
+        image.save(bio, format="PNG")
+        image_data = bio.getvalue()
+
+        movie_layout.append([
+            sg.Image(data=image_data, key=f"image_{movie['Id']}", enable_events=True),
+            sg.Text(movie['Title'], font=("Helvetica", 12), text_color="white", background_color="#2a2a2a", key=f"title_{movie['Id']}"),
+            sg.Text(movie['Description'], font=("Helvetica", 10), text_color="white", background_color="#2a2a2a", visible=False, key=f"description_{movie['Id']}")
+        ])
+
+    layout.append([
+        sg.Column(movie_layout, size=(800, 400), scrollable=True, vertical_scroll_only=True)
+    ])
+
+    window = sg.Window("Movie Search", layout, icon=icon_path, size=(1080,720), background_color="#2a2a2a", finalize=True)
+    window['search_term'].bind("<Return>", "_Enter")
+    frame = sg.tk.Frame(window.TKroot, padx=0, pady=0, bd=0, bg=sg.theme_background_color())
+    frame.place(x=0, y=0)
+    window['search_term'].Widget.master.place(in_=frame, x=10, y=55)
 
     while True:
         event, values = window.read()
 
         if event == sg.WINDOW_CLOSED:
             break
-        elif event == "Search":
-            search_term = values['search_term']
-            results = await search_movies(search_term)
+        elif event == "search_term" + "_Enter":
+            search_term = values['search_term'].strip()
+            results = await search_movies(search_term, None)
 
             if results:
-                await display_results(results)
+                display_results(results)
             else:
-                sg.popup("No results found!")
+                sg.popup("No results found!", background_color="#2a2a2a")
+        elif event.startswith("image_"):
+            movie_id = event.split("_")[1]
+            selected_movie = next(movie for movie in results if movie['Id'] == movie_id)
+            display_movie_details(selected_movie)
 
     window.close()
 
